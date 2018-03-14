b046d72f9423e2d245abc10c4c558eda728b6104
Ryan <ryzaar@hotmail.com>
2018-03-15 00:33:04 +1030

    Additional logging + fix swap fstab

--- install.sh
+++ install.sh
@@ -7,13 +7,17 @@ PROFITTRAILER_DOWNLOAD=https://github.com/taniman/profit-trailer/releases/downlo
 SWAP_ENABLED=false
 SWAP_SIZE=1G
 
+ln -s /tmp/firstboot.log ${PROFITTRAILER_HOME}/install-in-progress
+
 # ------------------------------------------------
 # Download ProfitTrailer
 # ------------------------------------------------
 
+echo "Adding user"
 useradd --system --user-group --create-home -K UMASK=0022 --home $PROFITTRAILER_HOME $PROFITTRAILER_USER
+
+echo "Downloading ProfitTrailer"
 curl -L -o /tmp/ProfitTrailer.zip $PROFITTRAILER_DOWNLOAD
-ln -s /tmp/firstboot.log ${PROFITTRAILER_HOME}/install-in-progress
 
 # ------------------------------------------------
 # Install and update packages
@@ -26,6 +30,7 @@ ln -s /tmp/firstboot.log ${PROFITTRAILER_HOME}/install-in-progress
 UPGRADE_ATTEMPT_COUNT=100
 UPGRADE_STATE=1
 
+echo "Installing dependencies"
 for i in `seq 1 $UPGRADE_ATTEMPT_COUNT`;
 do
     if [[ "$UPGRADE_STATE" -eq "1" ]]; then
@@ -62,12 +67,14 @@ fi
 # Complete Install
 # ------------------------------------------------
 
+echo "Unzipping ProfitTrailer"
 unzip -j /tmp/ProfitTrailer.zip -d $PROFITTRAILER_HOME
+
+echo "Updating PM2 configuration"
 sed -i 's/"cwd": "."/"cwd": "'$PROFITTRAILER_HOME'"/' $PROFITTRAILER_HOME/pm2-ProfitTrailer.json
 sed -i 's/"autorestart": false/"autorestart": true,\n      "error_file": "'$PROFITTRAILER_HOME'\/logs\/error.log",\n      "out_file": "'$PROFITTRAILER_HOME'\/logs\/out.log"/' $PROFITTRAILER_HOME/pm2-ProfitTrailer.json
 
-rm -f /tmp/ProfitTrailer.zip $PROFITTRAILER_HOME/install-in-progress
-
+echo "Adjusting file permissions"
 chown -R $PROFITTRAILER_USER:$PROFITTRAILER_USER $PROFITTRAILER_HOME
 chmod +x $PROFITTRAILER_HOME/ProfitTrailer.jar
 
@@ -81,9 +88,13 @@ chmod +x $PROFITTRAILER_HOME/ProfitTrailer.jar
 # ------------------------------------------------
 
 if [[ "${SWAP_ENABLED}" == "true" ]]; then
+    echo "Allocating swap"
     sudo fallocate -l $SWAP_SIZE /swapfile
     sudo chmod 600 /swapfile
     sudo mkswap /swapfile
     sudo swapon /swapfile
-    echo '/swapfile none swap sw 0 0'
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
+
+echo "Install complete"
+rm -f /tmp/ProfitTrailer.zip $PROFITTRAILER_HOME/install-in-progress
